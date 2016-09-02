291f96c5059edc475f261e2ce702abd308c31aca
Michaël Carrette <miccarr@me.com>
2016-09-02 02:04:49 +0200

    model refacto

---
+++ models.js
@@ -0,0 +1,52 @@
+const Database = require( './lib/findDB' ),
+    Log = require( './lib/methods/log.js' ),
+    Event = require( './lib/methods/events.js' );
+
+Database.on( 'log', Log.silly );
+
+module.exports = new Proxy( {}, {
+
+    get( t, property ) {
+
+        if ( typeof property != 'string' || property == 'inspect' || property == 'valueOf' )
+            return undefined;
+
+        property = property.toLowerCase();
+
+        if ( !Database.models[ property ] ) {
+            Database.define( property, {
+                createdAt: Date,
+                updatedAt: Date
+            } );
+            let model = Database.models[ property ];
+            model.belongsTo( 'user', {
+                as: 'createdBy',
+                foreignKey: 'createdBy'
+            } );
+            model.belongsTo( 'user', {
+                as: 'updatedBy',
+                foreignKey: 'updatedBy'
+            } );
+            model.beforeCreate = ( next, data ) => {
+                data.createdAt = new Date();
+                next();
+            };
+            model.beforeUpdate = ( next, data ) => {
+                data.updatedAt = new Date();
+                next();
+            };
+            model.afterSave = ( next ) => {
+                Event.emit( 'save:' + property, this );
+                next();
+            };
+        }
+
+        return Database.models[ property ];
+
+    },
+
+    set( t, property, definition ) {
+        return Database.define( property, definition );
+    }
+
+} );
