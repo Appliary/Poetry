587dc19e981603718a218c8ad7b76f91c7997d6b
Michaël Carrette <miccarr@me.com>
2016-09-09 11:25:33 +0200

    New ORM

--- models.js
+++ models.js
@@ -1,1 +1,25 @@
-module.exports = require('./lib/models');
+const Mongo = require( 'promised-mongo' ),
+    Log = require('./lib/methods/log'),
+    local = 'localhost:27017/database';
+
+// Are the env. variables set ?
+let url = process.env.DATABASE_PORT || process.env.database_port;
+if ( url ) url += '/database';
+else url = process.env.DATABASE || process.env.database;
+
+// Otherwise, localhost default config
+url = local;
+
+Log.info('Using MongoDB', url);
+
+module.exports = new Proxy( Mongo( url ), {
+    get( db, model ) {
+        if ( typeof model !== 'string' ) return undefined;
+
+        model = model.toLowerCase();
+        if ( model.charAt( model.length - 1 ) != 's' )
+            model += 's';
+
+        return db[ model ];
+    }
+} );
