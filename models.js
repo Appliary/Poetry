49a3507f200fb233c49bab678b32831dd8850cff
Michaël Carrette <miccarr@me.com>
2016-09-12 15:20:20 +0200

    MongoReplica

--- models.js
+++ models.js
@@ -1,17 +1,29 @@
 const Mongo = require( 'promised-mongo' ),
     Log = require( './lib/methods/log' ),
-    local = 'localhost:27017/database';
+    local = 'localhost/database';
 
 // Are the env. variables set ?
-let url = process.env.DATABASE_PORT || process.env.database_port;
-if ( url ) url += '/database';
-else url = process.env.DATABASE || process.env.database;
+let url = process.env.DATABASE || process.env.database;
+
+// Try the default rancher config
+if ( !url ) {
+    if (
+        process.env.MONGODB_MONGO_CLUSTER_1_PORT &&
+        process.env.MONGODB_MONGO_CLUSTER_2_PORT && process.env.MONGODB_MONGO_CLUSTER_3_PORT
+    ) url = 'mongodb://mongodb_mongo-cluster_1,mongodb_mongo-cluster_2,mongodb_mongo-cluster_3/database?replicaSet=' + ( process.env.replicaset || 'rs0' );
+    else {
+        url = process.env.DATABASE_PORT || process.env.database_port;
+        if ( url ) {
+            url += '/database';
+            if ( ~url.indexOf( '://' ) )
+                url = url.split( '://', 2 )[ 1 ];
+        }
+    }
+}
+
 
 // Otherwise, localhost default config
 if ( !url ) url = local;
-
-if( ~url.indexOf('://') )
-    url = url.split('://', 2)[1];
 
 Log.info( 'Using MongoDB', url );
 
